bd395433b71f8fb6354c319225db9b6cfa9f5b87
Nikita Chagochkin <nick.chagochkin@yandex.ru>
2022-10-30 15:49:23 +0300

    array basics

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -1,4 +1,35 @@
-import UIKit
+import Foundation
 
-var someIntArr = [1, 2, 3, 4, 5, 6, 7]
-// test comment
+var europeanCapitals = ["Madrid", "London", "Moscow", "Paris", "Berlin"]
+let amountOfCapitals = europeanCapitals.count
+
+europeanCapitals.append("Lisbon")
+europeanCapitals.append(contentsOf: ["Tallin", "Riga"])
+
+europeanCapitals += ["Helsinki"]
+europeanCapitals += ["Oslo", "Stockholm"]
+
+europeanCapitals.insert("Copenhagen", at: 8)
+
+var firstElementByProperty = europeanCapitals.first
+var firstElementBySubscript = europeanCapitals[0]
+var secondAndFourthElements = europeanCapitals[1...3]
+var lastElementByProperty = europeanCapitals.last
+
+europeanCapitals.insert("Wroclaw", at: 9)
+var wrongElementIndex = europeanCapitals.firstIndex(of: "Wroclaw")
+
+if let wrongElementIndex = wrongElementIndex {
+  var deletedWrongElement = europeanCapitals.remove(at: wrongElementIndex)
+}
+
+let removedFirstElement = europeanCapitals.removeFirst()
+let removedLastElement = europeanCapitals.removeLast()
+
+for capital in europeanCapitals {
+  print(capital)
+}
+
+for (index, value) in europeanCapitals.enumerated() {
+  print("Index: \(index), value: \(value)")
+}
